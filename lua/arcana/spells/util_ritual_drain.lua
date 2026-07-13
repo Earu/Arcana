f315c4d1313b97ffd9bc9869e0479c3de86598de
Ryan <ryan.rbsr@gmail.com>
2026-07-13 18:26:02 +0200

    buff to 50% of total time

--- lua/arcana/spells/util_ritual_drain.lua
+++ lua/arcana/spells/util_ritual_drain.lua
@@ -1,7 +1,7 @@
 -- Ritual Drain
 -- Siphon time off any ritual you aim at, cutting its remaining duration.
 -- Repeated casts collapse the ritual entirely, so rituals must be defended.
-local DRAIN_FRACTION = 0.20 -- fraction of the ritual's TOTAL lifetime removed per cast
+local DRAIN_FRACTION = 0.50 -- fraction of the ritual's TOTAL lifetime removed per cast
 local RANGE = 1500
 
 if SERVER then
